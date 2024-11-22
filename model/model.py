cfb85321bd0be4d6660a37066d8ce42b27b7a7d8
Giovanni Giordano <s331574@studenti.polito.it>
2024-11-22 20:01:56 +0100

    fixed the model

--- model/model.py
+++ model/model.py
@@ -1,29 +1,29 @@
 import pandas as pd
 from datasets import Dataset
 from sklearn.metrics import accuracy_score
-from transformers import BertForSequenceClassification, Trainer, TrainingArguments
+from transformers import AlbertForSequenceClassification, Trainer, TrainingArguments
 import torch
 
 # 1. Load the pre-tokenized dataset
 def load_pre_tokenized_data(csv_path):
-    # read the CSV
+    # Read the CSV
     df = pd.read_csv(csv_path)
     # Converts the columns into lists
     df['input_ids'] = df['input_ids'].apply(eval)  # Converts the string into a list (input_ids)
     df['attention_mask'] = df['attention_mask'].apply(eval)  # Converts the string into a list (attention_mask)
     if 'label' in df.columns:
         df['label'] = df['label'].apply(eval if str(df['label']).startswith('[') else int)
-    # Create the dataset Hugging Face
+    # Create the Hugging Face dataset
     dataset = Dataset.from_pandas(df)
     return dataset
 
 # 2. Prepare the model
 def prepare_model(num_labels):
-    model = BertForSequenceClassification.from_pretrained("bert-base-uncased", num_labels=num_labels)
+    model = AlbertForSequenceClassification.from_pretrained("albert-base-v2", num_labels=num_labels)
     return model
 
-#3. define a function to compute the evaluation metrics (accuracy in our case)
-def compute_metrics(eval_pred): 
+# 3. Define a function to compute the evaluation metrics (accuracy in our case)
+def compute_metrics(eval_pred):
     logits, labels = eval_pred
     predictions = torch.argmax(torch.tensor(logits), dim=-1)  # Get the predicted class
     accuracy = accuracy_score(labels, predictions)  # Compute accuracy
@@ -58,25 +58,22 @@ def train_model(train_dataset, val_dataset, model, output_dir):
     eval_results = trainer.evaluate()
     print("Evaluation Results:", eval_results)  # This will include the accuracy
 
-
-# 4. Main
+# 5. Main
 def main():
-    csv_path = "./tokenized_dataset_train.csv"  # path to the tokenized dataset
+    csv_path = "./tokenized_dataset_train.csv"  # Path to the tokenized dataset
     dataset = load_pre_tokenized_data(csv_path)
 
-    # split in training e validation set
+    # Split into training and validation sets
     dataset = dataset.train_test_split(test_size=0.2, seed=42)
     train_dataset = dataset["train"]
     val_dataset = dataset["test"]
 
-    # calculate the number of classes
-    num_labels = len(set(train_dataset['label']))  # for multi-class
+    # Calculate the number of classes
+    num_labels = len(set(train_dataset['label']))  # For multi-class
     model = prepare_model(num_labels)
 
-    # training
-    train_model(train_dataset, val_dataset, model, output_dir="./bert_model")
-
-    
+    # Training
+    train_model(train_dataset, val_dataset, model, output_dir="./albert_model")
 
 if __name__ == "__main__":
     main()
